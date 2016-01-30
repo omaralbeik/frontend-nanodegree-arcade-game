54aaffb5e8508a13145fc3bc5721e6079170427f
Omar Albeik <omaralbeik@gmail.com>
2016-01-30 12:43:12 +0200

    fix: player handleInput function is working, many other bug fixed

--- js/app.js
+++ js/app.js
@@ -1,19 +1,59 @@
+
+// constants
+var kCanvas = {
+    "width":505,
+    "height":606,
+    "grid": {
+        "rows":6,
+        "columns": 5
+    },
+    "block": {
+        "width":101,
+        "height":83
+    }
+};
+
+// game status
+var gameStarted = false;
+var gameOver = false;
+
+var kEnemies = {
+    "maxCount":3,
+    "startingX":0,
+    "startingY":60
+};
+
+var kPlayer = {
+    "startingX":202,
+    "startingY":386,
+    "lives":3
+};
+
+// helpers
+// createRandomNumber function creates a random number between min and max included:
+var randomNumber = function(min, max) {
+   return number = Math.floor(Math.random() * (max+1 - min)) + min;
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = randomNumber(1, 5);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    // by multiplying any movement by the dt parameter
+    // we ensure the game will run at the same speed for
     // all computers.
+    this.x  = this.x + 100 * dt * this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,10 +61,83 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+function createEnemies() {
+   for (var i=0;i<kEnemies.maxCount; i++) {
+      var randomLine = randomNumber(0,3);
+      var randomSpeed = randomNumber(1,5);
+      var enemy = new Enemy(kEnemies.startingX, kEnemies.startingY + (kCanvas.block.height * randomLine), randomSpeed);
+      allEnemies.push(enemy);
+   }
+}
+
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    this.currentBlock = {
+        "x":2,
+        "y":0
+    }
+    this.lives = kPlayer.lives;
+};
+
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+
+  switch (key) {
+
+    case 'right':
+        if (this.currentBlock.x < 4) {
+            console.log(key)
+            this.x += kCanvas.block.width;
+            this.currentBlock.x++;
+        };
+        break;
+
+    case 'left':
+        if (this.currentBlock.x > 0) {
+            console.log(key)
+            this.x -= kCanvas.block.width;
+            this.currentBlock.x--;
+        };
+        break;
+
+    case 'up':
+        if (this.currentBlock.y < 5) {
+            console.log(key)
+            this.y -= kCanvas.block.height;
+            this.currentBlock.y++;
+        };
+        break;
+
+    case 'down':
+        if (this.currentBlock.y > 0) {
+            console.log(key)
+            this.y += kCanvas.block.height;
+            this.currentBlock.y--;
+        };
+
+        break;
+
+    default:
+        break;
+  }
+
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -34,13 +147,23 @@ Enemy.prototype.render = function() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37:'left',
+        38:'up',
+        39:'right',
+        40:'down',
+        32:'space' // for jump
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+var player = new Player(kPlayer.startingX, kPlayer.startingY);
+// All enemy objects  are places in allEnemies array
+var allEnemies = [];
+createEnemies();
+
+
+
--- js/engine.js
+++ js/engine.js
@@ -25,8 +25,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = kCanvas.width;
+    canvas.height = kCanvas.height;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -109,14 +109,14 @@ var Engine = (function(global) {
          */
         var rowImages = [
                 'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/stone-block.png',   // Row 1 of 4 of stone
+                'images/stone-block.png',   // Row 2 of 4 of stone
+                'images/stone-block.png',   // Row 3 of 4 of stone
+                'images/stone-block.png',   // Row 4 of 4 of stone
+                'images/grass-block.png'    // Row of grass
             ],
-            numRows = 6,
-            numCols = 5,
+            numRows = kCanvas.grid.rows,
+            numCols = kCanvas.grid.columns,
             row, col;
 
         /* Loop through the number of rows and columns we've defined above
@@ -132,7 +132,7 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                ctx.drawImage(Resources.get(rowImages[row]), col * kCanvas.block.width, row * kCanvas.block.height);
             }
         }
 
